8e6f43358785c3136b79fec2439bb710c70f4bed
iAmTopeTemi <temitopett@yahoo.com>
2022-11-11 12:02:13 -0700

    checkov

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "testingJenkinsWithVamsi" {
     ami = var.ami_id
     instance_type = var.instance_type
     monitoring = true
+    encrypted     = true
     
     tags = {
         Name = "airbnb-infra"
